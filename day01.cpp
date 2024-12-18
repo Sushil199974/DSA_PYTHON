661c8a5ebda245045681ffbe5f8983250f32ff31
sushil choudhary <sushilchoudhary19992gmail.com>
2024-12-18 11:43:21 +0530

    day1

---
+++ day01.cpp
@@ -0,0 +1,53 @@
+/*#include <iostream> 
+using  namespace  std ; 
+int  main () 
+             {
+cout << "Hello CoderArmy" ; 
+return  0 ;
+            };*/
+
+
+ /*#include  <iostream> 
+ using  namespace  std  ; 
+ int  main  () 
+            { 
+ // integer 
+ int  num  =  500  ; 
+ cout  <<  num; 
+ return  0  ; 
+        };*/
+ 
+ /*#include  <iostream> 
+ using  namespace  std  ; 
+ int  main  () 
+            { 
+ // Character 
+ char  c  = 'a' ; 
+ cout  << c; 
+ return  0  ; 
+    };*/
+
+//  /*#include  <iostream> 
+//  using  namespace  std  ; 
+//  int  main  () 
+//              { 
+//  // double 
+//  double  num  =  93.3265  ; 
+//  cout  <<  num; 
+//  return  0  ; 
+//          };*/
+ 
+
+//    #include  <iostream> 
+//  using  namespace  std  ; 
+//  int  main  () 
+//         { 
+//  // float 
+//  float  d  =  1.32  ; 
+//  cout  <<  sizeof(d); 
+//  return  0  ; 
+//  }; 
+
+
+
+
